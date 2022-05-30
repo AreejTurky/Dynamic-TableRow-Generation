ff88212cb2009848cb569c4ee00eac6d836f4ad0
Areej AlOtaibi <72670290+AreejTurky@users.noreply.github.com>
2022-05-31 00:40:17 +0300

    Update Way#2.java

--- Way#2.java
+++ Way#2.java
@@ -38,6 +38,20 @@ TableLayout t1= (TableLayout)findViewById(R.id.tableLayout);
         r1.addView(v1);
         r1.addView(v2);
         t1.addView(r1 ,  new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
+        
+        //The bellow example will show how to generate with multi #
+        int i=0;
+        while (i<3)                 //Assuming the number i need to generate is 3
+        {
+        v2= new TextView(this);
+        v2.setTextColor(Color.WHITE); // set the color
+        v2.setPadding(5, 5, 5, 5);
+        v2.setText("HI AGAIN !!");
+        v2.setBackgroundColor(Color.BLACK);
+           //Hint, you can minimze this step and create layout file that has this style of TextView
+        r1.addView(v2);
+        t1.addView(r1 ,  new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.MATCH_PARENT));     
+        }
  }
 }
 //The difference is this solution shows the views next to each other 
